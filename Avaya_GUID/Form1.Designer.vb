8a7e820bccc75185cc5921beb4386e90ece57df0
Mudla <SmartGit@mudla.net>
2017-06-15 21:11:18 +0200

    Start

---
+++ Avaya_GUID/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1045, 595)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "&Einlesen"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(1207, 595)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "E&xit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(895, 34)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(372, 544)
+        Me.TextBox1.TabIndex = 2
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(432, 606)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(450, 12)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(421, 606)
+        Me.DataGridView2.TabIndex = 4
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(1126, 595)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "&Ausgeben"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1294, 630)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Avaya_GUID/Form1.vb
@@ -0,0 +1,122 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim DT2 As New DataTable
+
+
+    Private Sub test3()
+        Dim tb1 As TextBox = TextBox1
+        Dim DS As New DataSet()
+        Dim fs As FileStream
+        Dim sr As StreamReader
+        Dim DR As DataRow
+        Dim DT As DataTable
+
+        tb1.Clear()
+
+        fs = New FileStream("c:\GUID.xml", FileMode.Open, FileAccess.Read) ' --- Datei öffnen
+        sr = New StreamReader(fs) ' --- Datei einlesen
+        DS.ReadXml(sr) ' --- Stream in DataSet einlesen
+
+        DT = DS.Tables("Extension")
+
+        Dim column As New DataColumn
+        Dim row As DataRow
+
+        column = New DataColumn
+        column.DataType = System.Type.GetType("System.Boolean")
+        column.ColumnName = "Use"
+        DT2.Columns.Add(column)
+
+        column = New DataColumn
+        column.DataType = System.Type.GetType("System.Int16")
+        column.ColumnName = "ExtNo"
+        DT2.Columns.Add(column)
+
+        column = New DataColumn
+        column.DataType = System.Type.GetType("System.String")
+        column.ColumnName = "Guid"
+        DT2.Columns.Add(column)
+
+        With DT
+            For Each DR In .Rows
+                row = DT2.NewRow()
+                row("Use") = True
+                row("ExtNo") = DR.Item((DT.Columns.IndexOf("ExtNo")))
+                row("Guid") = DR.Item((DT.Columns.IndexOf("GUID")))
+                DT2.Rows.Add(row)
+            Next
+        End With
+
+        DT2.DefaultView.Sort = "ExtNo ASC"
+        DT2 = DT2.DefaultView.ToTable
+
+        Dim dgv1 = DataGridView1
+        dgv1.DataSource = DT2
+
+        Button3.Enabled = True
+    End Sub
+
+    Private Sub Ausgabe_Tabelle2()
+        'DT2.AcceptChanges()
+
+        Dim dgv2 = DataGridView2
+        Dim DT As New DataTable
+        Dim column As New DataColumn
+        Dim row As DataRow
+
+        column = New DataColumn
+        column.DataType = System.Type.GetType("System.Int16")
+        column.ColumnName = "ExtNo"
+        DT.Columns.Add(column)
+
+        column = New DataColumn
+        column.DataType = System.Type.GetType("System.String")
+        column.ColumnName = "Guid"
+        DT.Columns.Add(column)
+
+        For Each DR As DataRow In DT2.Rows
+            If DR("Use") Then
+                row = DT.NewRow()
+                row("ExtNo") = DR.Item((DT2.Columns.IndexOf("ExtNo")))
+                row("Guid") = DR.Item((DT2.Columns.IndexOf("GUID")))
+                DT.Rows.Add(row)
+            End If
+        Next
+        dgv2.DataSource = DT
+        TextBox1.Clear()
+
+        TextBox1.AppendText("[IP_OFFICE]" & vbCrLf)
+        TextBox1.AppendText("host = localhost" & vbCrLf)
+        TextBox1.AppendText("port = 8085" & vbCrLf)
+        TextBox1.AppendText("[USERS]" & vbCrLf)
+
+        For Each DR As DataRow In DT2.Rows
+            If DR("Use") = True Then
+                TextBox1.AppendText(DR.Item("ExtNo") & "=" & DR.Item("GUID") & vbCrLf)
+            End If
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        test3()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Ausgabe_Tabelle2()
+    End Sub
+
+    Private Sub DataGridView1_MouseUp(sender As Object, e As MouseEventArgs) Handles DataGridView1.MouseUp
+        'DT2.AcceptChanges()
+        DataGridView1.DataSource = DT2
+        Ausgabe_Tabelle2()
+    End Sub
+
+    Private Sub DataGridView1_MouseDown(sender As Object, e As MouseEventArgs) Handles DataGridView1.MouseDown
+        Ausgabe_Tabelle2()
+    End Sub
+End Class
